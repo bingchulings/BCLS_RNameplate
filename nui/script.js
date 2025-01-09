2cc5056a940e70df85aa5568b31335e432c6bd72
LowJhin <47059530+VirusAbility@users.noreply.github.com>
2025-01-09 15:15:38 +0900

    Update script.js

--- nui/script.js
+++ nui/script.js
@@ -1,1 +1,16 @@
-// add code
+window.addEventListener('message', (event) => {
+    const data = event.data;
+
+    if (data.type === "updateInfo") {
+        const infoBox = document.getElementById("info-box");
+        const job = document.getElementById("job");
+        const name = document.getElementById("name");
+
+        job.textContent = `${data.job} - ${data.grade}`;
+        name.textContent = data.playerName;
+
+        infoBox.style.left = `${data.x}px`;
+        infoBox.style.top = `${data.y}px`;
+        infoBox.style.display = data.visible ? "block" : "none";
+    }
+});
