f32d3889de22c83f0d5fc46bbd0d17420980fb3c
LowJhin <47059530+VirusAbility@users.noreply.github.com>
2025-01-09 15:11:55 +0900

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,14 @@
--- add code file
+fx_version 'cerulean'
+game 'rdr3'
+author 'BCLS'
+description 'Have the name of the job and character displayed on the player\'s head.'
+
+ui_page 'nui/index.html'
+
+files {
+    'nui/index.html'
+    'nui/style.css'
+    'nui/script.js'
+}
+
+client_scripts 'client.lua'
